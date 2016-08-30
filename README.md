0363d906ed650c928194a7845a6df94fd98aa76b
KENJU <ken901waga@gmail.com>
2016-08-31 05:54:11 +0900

    - Add app state

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
+# Tutorial
+
+* [official-tutorial](https://facebook.github.io/react/docs/tutorial.html)
 * [thiking-in-react](https://facebook.github.io/react/docs/thinking-in-react.html)
+
+# React Important Notes
+
+* React's one-way data flow (also called one-way binding) keeps everything modular and fast
+* props vs state
+    * `props` : props are a way of passing data from parent to child
+    * `state` : state triggers changes to your underlying data model
--- thinking-in-react/scripts/base.js
+++ thinking-in-react/scripts/base.js
@@ -18,7 +18,7 @@ var ProductRow = React.createClass({
             this.props.product.name :
             <span style={{color: 'red'}}>
                 {this.props.product.name}
-                </span>;
+            </span>;
         return (
             <tr>
                 <td>{name}</td>
@@ -33,14 +33,15 @@ var ProductTable = React.createClass({
         var rows = [];
         var lastCategory = null;
         this.props.products.forEach(function (product) {
+            if (product.name.indexOf(this.props.filterText) === -1 || (!product.stocked && this.props.inStockOnly)) {
+                return;
+            }
             if (product.category !== lastCategory) {
-                {
-                    rows.push(<ProductCategoryRow category={product.category} key={product.category}/>);
-                }
+                rows.push(<ProductCategoryRow category={product.category} key={product.category}/>);
             }
             rows.push(<ProductRow product={product} key={product.name}/>);
             lastCategory = product.category;
-        });
+        }.bind(this));
         return (
             <table>
                 <thead>
@@ -59,9 +60,9 @@ var SearchBar = React.createClass({
     render: function () {
         return (
             <form>
-                <input type="text" placeholder="Search..."/>
+                <input type="text" placeholder="Search..." value={this.props.filterText} />
                 <p>
-                    <input type="checkbox"/>
+                    <input type="checkbox" checked={this.props.inStocksOnly} />
                     {' '}
                     Only show products in stock
                 </p>
@@ -71,11 +72,24 @@ var SearchBar = React.createClass({
 });
 
 var FilterableProductTable = React.createClass({
+    getInitialState(): function() {
+        return {
+            filterText: '',
+            inStockOnly: false
+        };
+    },
     render: function () {
         return (
             <div>
-                <SearchBar />
-                <ProductTable products={this.props.products}/>
+                <SearchBar
+                    filterText={this.state.filterText}
+                    inStockOnly={this.state.inStockOnly}
+                />
+                <ProductTable
+                    products={this.props.products}
+                    filterText={this.state.filterText}
+                    inStockOnly={this.state.inStockOnly}
+                />
             </div>
         );
     }
